572c52d01b8b6bd59cf7c4f25cfaa8000db02da4
ranks72 <ranksdead.id@gmail.com>
2022-04-13 14:13:28 +0700

    ubah dikit

--- User_controller/Login.php
+++ User_controller/Login.php
@@ -3,7 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers");
 
     $jsondata = json_decode(file_get_contents("php://input"));
 
--- User_controller/Loginv2.php
+++ User_controller/Loginv2.php
@@ -3,7 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json");
     header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers");
 
     if (!empty($_POST['username']) && !empty($_POST['password'])) {
         
