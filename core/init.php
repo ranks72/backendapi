a8fd3bf83210c5af1eaae3c0eb70913bba1c392d
ranks72 <ranksdead.id@gmail.com>
2022-05-28 22:39:10 +0700

    rename lagi

--- core/init.php
+++ core/init.php
@@ -1,6 +1,6 @@
 <?php
     require_once "../config.php";
     require_once "../model/user/User_model.php";
-    require_once "../model/kategori/KategoriModel.php";
+    require_once "../model/Subkategori/SubKategoriModel.php";
     require_once "../helper/dd_helper.php";
 ?>
